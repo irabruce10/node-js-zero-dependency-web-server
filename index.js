ddb95819253d4b179d1a22df75a53564ebb3316c
Bruce <irabruce10@gmail.com>
2024-09-12 17:32:45 +0200

    Creating server

---
+++ index.js
@@ -0,0 +1,10 @@
+import http from 'node:http';
+
+const port = 3000;
+const server = http
+  .createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  })
+  .listen(port);
