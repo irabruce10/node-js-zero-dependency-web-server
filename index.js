fc109ec413e973c7e0feb401de61ca28b15ea8c7
Bruce <irabruce10@gmail.com>
2024-09-12 18:05:10 +0200

    Creating server

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-import http from 'node:http';
+import { createServer } from 'node:http';
+import fs from 'node:fs';
+import path from 'node:path';
 
-const port = 3000;
-const server = http
-  .createServer((req, res) => {
+const hostname = '127.0.0.1';
+const port = 3001;
+const publicDir = path.join(process.cwd(), 'public');
+
+const server = createServer((req, res) => {
+  const filePath = path.join(publicDir, req.url);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('File not found');
+      return;
+    }
+
+    const extname = path.extname(filePath);
+    let contentType;
+
+    switch (extname) {
+      case '.html':
+        contentType = 'text/html';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.js':
+        contentType = 'application/javascript';
+        break;
+      case '.jpg':
+      case '.jpeg':
+        contentType = 'image/jpeg';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      default:
+        contentType = 'application/octet-stream';
+    }
+
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-  })
-  .listen(port);
+    res.setHeader('Content-Type', contentType);
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
