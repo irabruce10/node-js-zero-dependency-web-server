49f82ba58136167b8b08fd962ee3f4f46d79ddc6
Bruce <irabruce10@gmail.com>
2024-09-14 12:30:42 +0200

    Fixing some bugs

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// Importng necessary modules
 import { createServer } from 'node:http';
 import { readFile } from 'node:fs';
 import { join, extname as _extname } from 'node:path';
 
+// Creating a Server using createServer
+
 const server = createServer((req, res) => {
-  const filePath = join('public', req.url);
+  const filePath = join('public', req.url === '/' ? 'index.html' : req.url);
+
+  // Reading the file using readFile
 
   readFile(filePath, (err, data) => {
     if (err) {
@@ -12,7 +17,10 @@ const server = createServer((req, res) => {
       return;
     }
 
+    // Determining the content type based on the file extension
+
     const extname = _extname(filePath);
+
     let contentType;
 
     switch (extname) {
@@ -35,12 +43,13 @@ const server = createServer((req, res) => {
       default:
         contentType = 'application/octet-stream';
     }
-
+    // Sending the response
     res.writeHead(200, { 'Content-Type': contentType });
     res.end(data);
   });
 });
 
-server.listen(3001, () => {
-  console.log('Server running at http://localhost:3000/');
+// Starting the server
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
 });
